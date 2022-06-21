918f168a5d494d15e5a323a4f96ea1058c83f918
Joshua Davian <joshuadavian@gmail.com>
2022-06-21 14:03:16 +0700

    fetch user data & logout process

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,42 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+  const [user, setUser] = useState({});
+
+  const navigate = useNavigate();
+
+  // Token
+  const token = localStorage.getItem('token');
+
+  const fetchData = async () => {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    await axios
+      .get('http://auth-jwt-laravel.test/api/auth/me')
+      .then((response) => {
+        setUser(response.data);
+      });
+  };
+
+  useEffect(() => {
+    if (!token) {
+      navigate('/login');
+    }
+
+    fetchData();
+  });
+
+  const logoutHandler = async () => {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    await axios
+      .post('http://auth-jwt-laravel.test/api/auth/logout')
+      .then(() => {
+        localStorage.removeItem('token');
+        navigate('/login');
+      });
+  };
+
   return (
     <div className="container">
       <div className="d-flex align-items-center" style={{ height: '100vh' }}>
@@ -10,9 +46,12 @@ function Home() {
               <div className="card">
                 <div className="card-header">Home Page</div>
                 <div className="card-body">
-                  <h5>Hello, Joe</h5>
+                  <h5>Hello, {user.name}</h5>
+                  <h6 className="text-secondary">{user.email}</h6>
                   <p>Welcome to Simple React Auth App.</p>
-                  <button className="btn btn-danger">Logout</button>
+                  <button onClick={logoutHandler} className="btn btn-danger">
+                    Logout
+                  </button>
                 </div>
               </div>
             </div>
